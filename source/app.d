25e7d75f2a04057bbdd48eab2e6f82155497c5d7
Jaap Geurts <jaap.geurts@fontys.nl>
2022-09-27 12:56:58 +0200

    Improvements. Added gas and mqtt

--- source/app.d
+++ source/app.d
@@ -1,6 +1,7 @@
 import std.stdio;
 import std.conv;
 import std.array;
+import std.format;
 import std.datetime.systime : SysTime, Clock;
 
 import std.file : readText;
@@ -8,6 +9,21 @@ import std.file : readText;
 import dserial;
 
 import iec62056;
+
+import AsyncClient;
+import ConnectOptions;
+import Message;
+import Token;
+import MqttException : MqttException;
+import Callback;
+import ActionListener;
+
+private immutable TOPIC_ELECTRICITY_TOTAL_PEAK = "energy/electricity/totalpeak";
+private immutable TOPIC_ELECTRICITY_TOTAL_OFF = "energy/electricity/totaloff";
+private immutable TOPIC_ELECTRICITY_CURRENT = "energy/electricity/current";
+private immutable TOPIC_GAS_TOTAL = "energy/gas/total";
+private immutable TOPIC_GAS_CURRENT = "energy/gas/current";
+
 
 /* 
  * Description: Application to read ESMR-5 messages from smart energy meter
@@ -50,10 +66,33 @@ enum ParseState {
     CRC
 }
 
+private MqttAsyncClient client;
+private MqttConnectOptions opt;
+
 int main() {
 
     writeln("COSEM Smart meter decoder");
     writeln("©2022 Jaap Geurts");
+
+    // setup mqtt
+    string serverURI = "tcp://192.168.20.251:8883";
+    client = new MqttAsyncClient(serverURI, "energylogger");
+    if (!client.isOK()) {
+        stderr.writeln("Error creating MQTT client.");
+        return 1;
+    }
+
+    opt = new MqttConnectOptions("NhaMinh","ValleStap74#");
+    opt.getOptionsPtr().automaticReconnect = true;
+
+    try {
+        MqttToken tok = client.connect(opt, null, null);
+        tok.waitForCompletion();
+    }
+    catch (MqttException exc) {
+        stderr.writeln("Cannot connect: ", exc.getReasonCode());
+        return 1;
+    }
 
     DSerial serialPort = new DSerial(ttyName, 115200);
     serialPort.setBlockingMode(DSerial.BlockingMode.Blocking);
@@ -71,7 +110,7 @@ int main() {
             break;
         }
 
-        float totalPower;
+        float totalPower,totalGas;
         int currentPower;
         foreach (ref child; parseTree1.children[0]) {
             if (child.name == "IEC62056.line") {
@@ -81,10 +120,20 @@ int main() {
                 if (child.matches[0] == "1-0:1.7.0") {
                     currentPower = to!int(to!float(child.matches[1]) * 1000);
                 }
+                if (child.matches[0] == "0-1:24.2.1") {
+                    totalGas = to!float(child.matches[2]);
+                }
             }
         }
-        SysTime currentTime = Clock.currTime();
-        writeln(currentTime.toSimpleString(),": ",totalPower,"kWh, ",currentPower,"W");
+        // SysTime currentTime = Clock.currTime();
+        // writeln(currentTime.toSimpleString(),": ",totalPower,"kWh, ",currentPower,"W");
+
+        MqttDeliveryToken token = client.publish(TOPIC_ELECTRICITY_CURRENT, format("%d%s",currentPower,"W"), 1, true);
+        token.waitForCompletion();
+        token = client.publish(TOPIC_ELECTRICITY_TOTAL_PEAK, format("%f%s",totalPower,"kWh"), 1, true);
+        token.waitForCompletion();
+        token = client.publish(TOPIC_GAS_TOTAL, format("%f%s",totalGas,"m3"), 1, true);
+        token.waitForCompletion();
     }
     serialPort.close();
     return 1;
